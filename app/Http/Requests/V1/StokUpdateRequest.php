41296fe48352b8c0955813fde096aa1dd50d5ba4
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-12-28 11:17:04 +0700

    fix validation type for minimum stok in StokUpdateRequest

--- app/Http/Requests/V1/StokUpdateRequest.php
+++ app/Http/Requests/V1/StokUpdateRequest.php
@@ -23,7 +23,29 @@ class StokUpdateRequest extends FormRequest
     {
         return [
             'name' => 'sometimes|string|max:255',
-            'minimum_stok' => 'sometimes|integer|min:0'
+            'minimum_stok' => 'sometimes|numeric|min:0',
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.string' => 'Nama stok harus berupa teks.',
+            'name.max' => 'Nama stok maksimal 255 karakter.',
+
+            'minimum_stok.numeric' => 'Minimum stok harus berupa angka.',
+            'minimum_stok.min' => 'Minimum stok tidak boleh kurang dari 0.',
+        ];
+    }
+    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        throw new \Illuminate\Http\Exceptions\HttpResponseException(
+            response()->json([
+                'success' => false,
+                'message' => $validator->errors()->first(),
+                'errors' => $validator->errors(),
+                'data' => null,
+            ], 422)
+        );
     }
 }
