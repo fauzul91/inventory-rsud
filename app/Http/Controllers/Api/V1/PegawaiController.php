a9491cf114d11753fb4457c4d841c24a28a00e2e
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-12-30 20:16:08 +0700

    optimizing response helper and pegawai controller feature

--- app/Http/Controllers/Api/V1/PegawaiController.php
+++ app/Http/Controllers/Api/V1/PegawaiController.php
@@ -8,19 +8,15 @@ use App\Interfaces\V1\PegawaiRepositoryInterface;
 use App\Repositories\V1\PegawaiRepository;
 use App\Http\Requests\V1\PegawaiStoreRequest;
 use App\Http\Requests\V1\PegawaiUpdateRequest;
-use Exception;
 use Illuminate\Http\Request;
-use Throwable;
 
 class PegawaiController extends Controller
 {
     private PegawaiRepository $pegawaiRepository;
-
     public function __construct(PegawaiRepositoryInterface $pegawaiRepository)
     {
         $this->pegawaiRepository = $pegawaiRepository;
     }
-
     public function index(Request $request)
     {
         $filters = [
@@ -33,39 +29,33 @@ class PegawaiController extends Controller
         $pegawai = $this->pegawaiRepository->getAll($filters);
         return ResponseHelper::jsonResponse(true, 'Data pegawai berhasil diambil', $pegawai, 200);
     }
-
     public function getAllForSelect()
     {
-        $pegawai = $this->pegawaiRepository->getAllForSelect();
-        return ResponseHelper::jsonResponse(true, 'Data pegawai berhasil diambil', $pegawai, 200);
+        return ResponseHelper::jsonResponse(true, 'Data pegawai berhasil diambil', $this->pegawaiRepository->getAllForSelect());
     }
+
     public function getPegawaiForProfile()
     {
-        $pegawai = $this->pegawaiRepository->getAllPegawaiForProfile();
-        return ResponseHelper::jsonResponse(true, 'Data profil pegawai berhasil diambil', $pegawai, 200);
+        return ResponseHelper::jsonResponse(true, 'Data profil pegawai berhasil diambil', $this->pegawaiRepository->getAllPegawaiForProfile());
     }
 
     public function store(PegawaiStoreRequest $request)
     {
-        $pegawai = $this->pegawaiRepository->create($request->validated());
-        return ResponseHelper::jsonResponse(true, 'Pegawai berhasil ditambahkan', $pegawai, 201);
+        return ResponseHelper::jsonResponse(true, 'Pegawai berhasil ditambahkan', $this->pegawaiRepository->create($request->validated()), 201);
     }
 
     public function show($id)
     {
-        $pegawai = $this->pegawaiRepository->findById($id);
-        return ResponseHelper::jsonResponse(true, 'Detail pegawai berhasil diambil', $pegawai, 200);
+        return ResponseHelper::jsonResponse(true, 'Detail pegawai berhasil diambil', $this->pegawaiRepository->findById($id));
     }
 
     public function update(PegawaiUpdateRequest $request, $id)
     {
-        $pegawai = $this->pegawaiRepository->update($request->validated(), $id);
-        return ResponseHelper::jsonResponse(true, 'Data pegawai berhasil diperbarui', $pegawai, 200);
+        return ResponseHelper::jsonResponse(true, 'Data pegawai berhasil diperbarui', $this->pegawaiRepository->update($request->validated(), $id));
     }
 
     public function toggleStatus($id)
     {
-        $pegawai = $this->pegawaiRepository->toggleStatus($id);
-        return ResponseHelper::jsonResponse(true, 'Status pegawai berhasil diperbarui', $pegawai, 200);
+        return ResponseHelper::jsonResponse(true, 'Status pegawai berhasil diperbarui', $this->pegawaiRepository->toggleStatus($id));
     }
 }
